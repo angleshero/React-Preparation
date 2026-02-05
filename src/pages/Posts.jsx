340fbe859bd20610c60a721e68468af6adb4bc0d
tommy <example@email.com>
2026-02-05 10:43:27 -0600

    homepage

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
 const Posts = () => {
+  let navigate = useNavigate();
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState();
   const [searchId, setSearchId] = useState('id');
 
   function onSearch() {
@@ -13,6 +14,7 @@ const Posts = () => {
   }
 
   async function fetchPosts(userId) {
+    setLoading(true);
     const { data }  = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`);
     setPosts(data);
     setLoading(false);
@@ -27,7 +29,10 @@ useEffect(() => {
   return (
     <>
   <div className="post__search">
-    <button>← Back</button>
+    
+    <button onClick={() => navigate(-1)}>← Back</button>
+    
+    
     <div className="post__search--container">
       <label className="post__search--label">Search by Id</label>
       <input type="number" 
